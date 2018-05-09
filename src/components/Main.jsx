bd17cd0c33195fd5f8d7454649f9361fd0beacd4
Karl Jóhann Jóhannsson <karljohann@datamarket.com>
2018-05-09 18:37:36 +0000

    Added Show-more functionality to questions list.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Question from './Question';
 import Pill from './Pill';
 import faqs from '../faqs.json';
 
-function Main() {
-  return (
-    <div className="flugleidir-main">
-      <div className="flugleidir-content flugleidir-main-header">
-        <div className="flugleidir-title">Answers</div>
-        <Pill
-          title="Ask question"
-        />
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      amountToShow: 7,
+      showMoreButton: true,
+    };
+    this.moreQuestions = this.moreQuestions.bind(this);
+  }
+
+  moreQuestions() {
+    const amountOfQuestions = faqs.length + 1;
+    const howManyMore = (amountOfQuestions > this.state.amountToShow + 7)
+      ? this.state.amountToShow + 7
+      : faqs.length;
+    this.setState({
+      amountToShow: howManyMore,
+      showMoreButton: (amountOfQuestions === howManyMore),
+    });
+  }
+
+  render() {
+    return (
+      <div className="flugleidir-main">
+        <div className="flugleidir-content flugleidir-main-header">
+          <div className="flugleidir-title">Answers</div>
+          <Pill
+            title="Ask question"
+          />
+        </div>
+        <div className="flugleidir-main-questions flugleidir-content">
+          {faqs.slice(0, this.state.amountToShow).map(faq => (
+            <Question key={faq.key} {...faq} />
+          ))}
+
+          {this.state.showMoreButton &&
+            <button
+              onClick={this.moreQuestions}
+              className="flugleidir-show-more-button"
+            >
+              View more Questions {/* TODO: Tranlsation strings */ }
+            </button>
+          }
+        </div>
       </div>
-      <div className="flugleidir-main-questions flugleidir-content">
-        {faqs.map(faq => (
-          <Question key={faq.key} {...faq} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Main;
